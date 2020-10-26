3b95d5d16fef6a2cac9c3d8a564de5615f039834
Anton Bubel <a.bubel@itransition.com>
2020-10-26 21:35:46 +0300

    Update main.js, check for TZ environment variable and show a varning if it's not provided

--- src/main.js
+++ src/main.js
@@ -3,16 +3,22 @@ const Bot = require('./telegram/bot');
 const Schedule = require('./telegram/schedule');
 
 const initializeRollCallBot = () => {
+  if (!process.env.TZ) {
+    const warningMessage = `NODE TZ environment variable is not set, the bot will consume server timezone instead.`;
+    console.warn(warningMessage);
+  } else {
+    const infoMessage = `The bot will use the timezone defined in the NODE TZ environement variable. TZ=${process.env.TZ}`;
+    console.info(infoMessage);
+  }
+
   if (!process.env.ROLL_CALL_BOT_CONNECTION_STRING) {
     const errorMessage = 'The connection string cannot be found, please specify the connection string in the environment variables, environment variable key: ROLL_CALL_BOT_CONNECTION_STRING';
-
     console.error(errorMessage);
     throw new Error(errorMessage);
   }
 
   if (!process.env.ROLL_CALL_BOT_TOKEN) {
     const errorMessage = 'Telegram bot token cannot be found, please specify the token in the environment variables, environment variable key: ROLL_CALL_BOT_TOKEN';
-
     console.error(errorMessage);
     throw new Error(errorMessage);    
   }
